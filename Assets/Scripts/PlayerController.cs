1bc41c5743aafa7d65d83c9b90bfd97041c16bc3
N1sgamm <N1sgamm@gmail.com>
2022-09-22 15:55:08 -0300

    completed section 5.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
 
     private Rigidbody rb;
-
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -16,6 +18,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
     }
 
     private void OnMove(InputValue movementValue)
@@ -24,6 +29,11 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 
     private void FixedUpdate()
@@ -37,7 +47,10 @@ public class PlayerController : MonoBehaviour
     {
         if(other.gameObject.CompareTag("PickUp"))
         {
-        other.gameObject.SetActive(false);
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
         }
     }
 }
